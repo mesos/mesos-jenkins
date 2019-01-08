9ec0321a9e95a3b8fa9acce58b6466ef8ef2d5cc
Till Toenshoff <toenshoff@me.com>
2019-01-08 22:42:00 +0100

    Removed obviously unlrelated setup from global configurations.

--- global-variables.ps1
+++ global-variables.ps1
@@ -1,3 +1,6 @@
+# TODO(tillt): Double-Check which of these are obsolete. Consider moving
+# hardcoded "dcos" artifacts into "mesos".
+
 # DCOS common configurations
 $STORAGE_SERVER_ADDRESS = "192.10.1.12"
 $STORAGE_SERVER_USER = "jenkins"
@@ -13,33 +16,10 @@ $MESOS_BUILD_OUT_DIR = Join-Path $MESOS_DIR "build-output"
 $MESOS_BUILD_LOGS_DIR = Join-Path $MESOS_BUILD_OUT_DIR "logs"
 $MESOS_BUILD_BINARIES_DIR = Join-Path $MESOS_BUILD_OUT_DIR "binaries"
 
-# dcos-net configurations
-$DCOS_NET_DIR = Join-Path $DCOS_DIR "dcos-net"
-$DCOS_NET_GIT_REPO_DIR = Join-Path $DCOS_NET_DIR "dcos-net"
-$DCOS_NET_LIBSODIUM_GIT_DIR = Join-Path $DCOS_NET_DIR "libsodium"
-$DCOS_NET_BUILD_OUT_DIR = Join-Path $DCOS_NET_DIR "build-output"
-$DCOS_NET_BUILD_RELEASE_DIR = Join-Path $DCOS_NET_BUILD_OUT_DIR "release"
-$DCOS_NET_BUILD_LOGS_DIR = Join-Path $DCOS_NET_BUILD_OUT_DIR "logs"
-
-# Diagnostics configurations
-$DIAGNOSTICS_DIR = Join-Path $DCOS_DIR "diagnostics"
-$DIAGNOSTICS_GIT_REPO_DIR = Join-Path $DIAGNOSTICS_DIR "src\github.com\dcos\dcos-diagnostics"
-$DIAGNOSTICS_BUILD_OUT_DIR = Join-Path $DIAGNOSTICS_DIR "build-output"
-$DIAGNOSTICS_BUILD_LOGS_DIR = Join-Path $DIAGNOSTICS_BUILD_OUT_DIR "logs"
-$DIAGNOSTICS_BUILD_BINARIES_DIR = Join-Path $DIAGNOSTICS_BUILD_OUT_DIR "binaries"
-
-# Metrics configurations
-$METRICS_DIR = Join-Path $DCOS_DIR "metrics"
-$METRICS_GIT_REPO_DIR = Join-Path $METRICS_DIR "src\github.com\dcos\dcos-metrics"
-$METRICS_BUILD_OUT_DIR = Join-Path $METRICS_DIR "build-output"
-$METRICS_BUILD_LOGS_DIR = Join-Path $METRICS_BUILD_OUT_DIR "logs"
-$METRICS_BUILD_BINARIES_DIR = Join-Path $METRICS_BUILD_OUT_DIR "binaries"
-
 # Installers & git repositories URLs
 $GIT_URL = "$STORAGE_SERVER_BASE_URL/downloads/git-64-bit.exe"
 $7ZIP_URL = "https://www.7-zip.org/a/7z1805-x64.msi"
 $GOLANG_URL = "https://dl.google.com/go/go1.9.4.windows-amd64.msi"
-$DCOS_WINDOWS_GIT_URL = "https://github.com/dcos/dcos-windows"
 $LIBSODIUM_GIT_URL = "https://github.com/jedisct1/libsodium"
 
 # Tools installation directories
